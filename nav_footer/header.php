40934f0afb9da7b8aefb0ee102494aeba2cf3484
Ria-nix <podkovadasha@gmail.com>
2022-04-05 18:43:22 +0200

    /././././

--- nav_footer/header.php
+++ nav_footer/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,35 +8,42 @@
     <title>header</title>
     <link rel="stylesheet" href="style_header/style.css">
 </head>
+
 <body>
     <div class="grid">
         <div class="main_wrap">
             <img src="../images/logo.svg" alt="logo">
             <div class="menu">
-                <a href="#">
-                    <span class="icon_link" id=""></span>
+                <a href="#" name="name_advantages">
+                    <span class="icon_link" id="advantages"></span>
                     <p class="item_txt">Преимущества</p>
                     <span class="underline"></span>
                 </a>
-                <a href="#">
-                    <span class="icon_link" id=""></span>
+                <a href="#" name="name_projects">
+                    <span class="icon_link" id="projects"></span>
                     <p class="item_txt">Проекты</p>
                     <span class="underline"></span>
                 </a>
-                <a href="#">
-                    <span class="icon_link" id=""></span>
+                <a href="#" name="name_townhouse">
+                    <span class="icon_link" id="townhouse"></span>
+                    <p class="item_txt">ТАУНХАУСЫ</p>
+                    <span class="underline"></span>
+                </a>
+                <a href="#" name="name_gallery">
+                    <span class="icon_link" id="gallery"></span>
                     <p class="item_txt">Галерея</p>
                     <span class="underline"></span>
                 </a>
             </div>
             <div class="big_text">
-                
+                <p>Ипотека <span>2.7</span>%</p>
             </div>
             <div class="btn_home">
-            <input type="submit" value="выбрать дом">
+                <input type="submit" value="выбрать дом">
             </div>
         </div>
     </div>
-    
+
 </body>
+
 </html>
