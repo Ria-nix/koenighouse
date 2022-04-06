1e73602c745a317edbe1ac07d906a1c449942cc7
Ria-nix <podkovadasha@gmail.com>
2022-04-06 16:39:18 +0200

    //////.

--- nav_footer/header.php
+++ nav_footer/header.php
@@ -10,40 +10,45 @@
 </head>
 
 <body>
-    <div class="grid">
+    <div class="layout_wrap layout_menu">
         <div class="main_wrap">
+
             <img src="../images/logo.svg" alt="logo">
-            <div class="menu">
-                <a href="#" name="name_advantages">
-                    <span class="icon_link" id="advantages"></span>
-                    <p class="item_txt">Преимущества</p>
-                    <span class="underline"></span>
-                </a>
-                <a href="#" name="name_projects">
-                    <span class="icon_link" id="projects"></span>
-                    <p class="item_txt">Проекты</p>
-                    <span class="underline"></span>
-                </a>
-                <a href="#" name="name_townhouse">
-                    <span class="icon_link" id="townhouse"></span>
-                    <p class="item_txt">ТАУНХАУСЫ</p>
-                    <span class="underline"></span>
-                </a>
-                <a href="#" name="name_gallery">
-                    <span class="icon_link" id="gallery"></span>
-                    <p class="item_txt">Галерея</p>
-                    <span class="underline"></span>
-                </a>
-            </div>
-            <div class="big_text">
-                <p>Ипотека <span>2.7</span>%</p>
-            </div>
-            <div class="btn_home">
-                <input type="submit" value="выбрать дом">
-            </div>
+            <span class="toggle_btn"></span>
+
+            <div class="sidebar">
+                <div class="menu">
+                    <a href="#" name="name_advantages">
+                        <span class="icon_link" id="advantages"></span>
+                        <p class="item_txt">Преимущества</p>
+                        <span class="underline"></span>
+                    </a>
+                    <a href="#" name="name_projects">
+                        <span class="icon_link" id="projects"></span>
+                        <p class="item_txt">Проекты</p>
+                        <span class="underline"></span>
+                    </a>
+                    <a href="#" name="name_gallery">
+                        <span class="icon_link" id="gallery"></span>
+                        <p class="item_txt">Галерея</p>
+                        <span class="underline"></span>
+                    </a>
+                    <!-- ======== don't touch ======== -->
+                    <!-- <a href="#" name="name_townhouse">
+                        <span class="icon_link" id="townhouse"></span>
+                        <p class="item_txt">ТАУНХАУСЫ</p>
+                        <span class="underline"></span>
+                    </a> -->
+                    <!-- ======== ======== ======== -->
+                </div>
+                <div class="big_text">
+                    <p>Ипотека <span>2.7</span>%</p>
+                </div>
+                <div class="btn_home">
+                    <input type="submit" value="выбрать дом">
+                </div>
+            </div>            
         </div>
     </div>
-
 </body>
-
 </html>
