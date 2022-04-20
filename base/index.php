a422187610f75c7a16cb51125ba25022aa11d3d6
Ria-nix <podkovadasha@gmail.com>
2022-04-20 17:13:18 +0200

    туц

--- base/index.php
+++ base/index.php
@@ -2,12 +2,51 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Live 1</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <div id="Wrapper">
+		<div id="header">
+			<div class="block1"></div>
+            <div class="block2"></div>
+            <div class="block3"></div>
+            <div class="block4"></div>
+            <div class="block5"></div>
+          
+
+		</div>
+		<div id="menu">
+			<div class="block1-1"></div>
+            <div class="block1-2"></div>
+            <div class="block1-3"></div>
+            <div class="block1-4"></div>
+            <div class="block1-5"></div>
+		</div>
+        <div id="content">
+            <div class="block1-6"></div>
+            <div class="block1-7"></div>
+            <div class="block1-8"></div>
+            <div class="block1-9"></div>
+            <div class="block1-10"></div>
+        </div>
+        <div id="content2">
+            <div class="block1-11"></div>
+            <div class="block1-12"></div>
+            <div class="block1-13"></div>
+            <div class="block1-14"></div>
+            <div class="block1-15"></div>
+        </div>
+        <divid="content3">
+            <div class="block1-16"></div>
+            <div class="block1-17"></div>
+            <div class="block1-18"></div>
+            <div class="block1-19"></div>
+            <div class="block1-20"></div>
+        </div>
+
+		
+	</div>
+        
 </body>
 </html>
